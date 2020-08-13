1327d0db3340796a11b7a197a9fb67581cdd6ea5
umairahmad125 <umairahmad125@gmail.com>
2020-08-13 18:41:29 +0500

    add linters configurations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,5 +3,4 @@ class Comment < ApplicationRecord
   belongs_to :user
 
   validates :comment_text, presence: true
-  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
+require 'bcrypt'
+
 class User < ApplicationRecord
+  include BCrypt
+
   has_many :posts
-  has_secure_password :password
+  before_save :secure_password
 
   validates :username, presence: true, uniqueness: { case_sensitive: false },
                        length: { minimum: 4, maximum: 12 }
@@ -8,4 +12,8 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze }
 
   validates :password, presence: true, length: { minimum: 4, maximum: 12 }
+
+  def secure_password
+    self.password = Password.create(password)
+  end
 end
