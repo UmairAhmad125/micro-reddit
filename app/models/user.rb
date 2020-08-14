64e1e1544ef54f8a27151f3168f8ca49227f23f1
umairahmad125 <umairahmad125@gmail.com>
2020-08-14 13:37:38 +0500

    correct password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,6 @@
-require 'bcrypt'
-
 class User < ApplicationRecord
-  include BCrypt
 
   has_many :posts
-  before_save :secure_password
 
   validates :username, presence: true, uniqueness: { case_sensitive: false },
                        length: { minimum: 4, maximum: 12 }
@@ -13,7 +9,5 @@ class User < ApplicationRecord
 
   validates :password, presence: true, length: { minimum: 4, maximum: 12 }
 
-  def secure_password
-    self.password = Password.create(password)
-  end
+  
 end
